f8024854654a60ef1c7143e342e97b1765c3f94a
ryandey <techrpd15113@gmail.com>
2023-04-09 16:57:29 -0400

    Radiata badge edits

--- components/Radiata.js
+++ components/Radiata.js
@@ -1,14 +1,18 @@
-const { Box } = require("@chakra-ui/react")
+const { Box, Text } = require("@chakra-ui/react")
 
 const Radiata = () => {
   return (
     <Box
-      bgGradient={"linear(to-tr, red, orange)"}
+      bgGradient={"linear(to-tr, gray.900, gray.500)"}
       px={3}
       py={1}
       borderRadius="md"
+      borderWidth="1px"
+      borderColor="gray.900"
     >
-      <p>Designed by RW</p>
+      <Text fontWeight="bold" color="white">
+        RW
+      </Text>
     </Box>
   )
 }
