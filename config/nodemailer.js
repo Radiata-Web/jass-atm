e40d03ffebafb555abc9b4407dc231de0dfb5fd3
Ryan Dey <techrpd15113@gmail.com>
2023-04-29 16:35:10 -0400

    Fix environment variable references

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer"
 
-const email = process.env.EMAIL
-const pass = process.env.PASSWORD
+const email = process.env.NEXT_PUBLIC_EMAIL
+const pass = process.env.NEXT_PUBLIC_PASSWORD
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
