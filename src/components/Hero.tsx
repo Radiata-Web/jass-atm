d8f3b9c60c6f2fd5deb427984ab4590276da3df6
Ryan Dey <ryandeyp@gmail.com>
2025-12-27 11:24:28 -0600

    Hero component + contact route

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,55 @@
 import { Phone, Send } from "lucide-react";
 import { Button } from "./ui/button";
+import { Link } from "@tanstack/react-router";
 
 export default function Hero() {
   return (
     <section className="bg-[url('/img/downtown-jax.webp')] bg-center lg:bg-cover">
-      <div className="flex flex-col gap-12 px-8 py-16 bg-linear-to-b from-black/50 to-black">
-        <div className="flex flex-col gap-4 max-w-4xl mx-auto">
-          <p className="text-5xl leading-tight font-semibold text-white">
-            Reliable ATM Solutions for Businesses Across the Southeast.
-          </p>
-          <p className="text-xl text-gray-300">
-            Jass ATM Sales & Service helps hotels, retailers, bars, and more
-            boost revenue with fully managed ATM programs, fast repairs, and
-            24/7 monitoring—without long‑term contracts or heavy fees.
-          </p>
-        </div>
+      {/* Background overlay */}
+      <div className="bg-linear-to-bl from-black/50 to-black">
+        {/* Content Container */}
+        <div className="grid gap-12 grid-cols-1 lg:grid-cols-2">
+          <div className="flex flex-col gap-12 max-w-[90%] xl:max-w-[60%] mx-auto py-12 md:py-24">
+            {/* Content */}
+            <div className="flex flex-col gap-4 max-w-4xl">
+              <p className="text-5xl leading-tight font-semibold text-white">
+                Reliable ATM Solutions for Businesses Across the Southeast.
+              </p>
+              <p className="text-xl text-gray-300 max-w-3xl">
+                Jass ATM Sales & Service helps hotels, retailers, bars, and more
+                boost revenue with fully managed ATM programs, fast repairs, and
+                24/7 monitoring—without long‑term contracts or heavy fees.
+              </p>
+            </div>
 
-        <div className="flex gap-4 mx-auto">
-          <Button size="lg">
-            <Send /> Request a Free ATM Consultation
-          </Button>
-          <Button variant="secondary" size="lg">
-            <Phone />
-            Call (800) 676-6838
-          </Button>
+            {/* CTA */}
+            <div className="flex flex-col gap-4 md:flex-row">
+              {/* Email Button */}
+              <Button size="lg" asChild>
+                <Link to="/contact">
+                  <Send /> Request a Free ATM Consultation
+                </Link>
+              </Button>
+
+              {/* Phone Button */}
+              <Button variant="outline" size="lg" asChild>
+                <a href="tel:8006766838">
+                  <Phone />
+                  Call (800) 676-6838
+                </a>
+              </Button>
+            </div>
+          </div>
+
+          {/* Illustration */}
+          <div className="items-center justify-center hidden lg:flex">
+            <img
+              src="/figures/maintenance.svg"
+              alt="Maintenance"
+              width={500}
+              className="mx-auto"
+            />
+          </div>
         </div>
       </div>
     </section>
